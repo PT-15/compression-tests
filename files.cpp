33472dc58d4f07dd764cdddcef33af30a491e4c1
P-15 <dbp-15@proton.me>
2024-03-04 19:40:37 +0100

    Add function to open files

---
+++ files.cpp
@@ -0,0 +1,18 @@
+#include "files.h"
+
+#include <fcntl.h>
+
+void open_files (std::string in_name, std::string out_name, int &in_fd, int &out_fd)
+{
+    in_fd = open(in_name.c_str(), O_RDONLY);
+    if (in_fd < 0) {
+        std::perror("Couldn't open input file");
+        exit(1);
+    }
+
+    out_fd = open(out_name.c_str(), O_CREAT | O_TRUNC | O_WRONLY, 0644);
+    if (out_fd < 0) {
+        std::perror("Couldn't open output file");
+        exit(1);
+    }
+}
---
+++ files.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <string>
+
+void open_files (std::string in_name, std::string out_name, int &in_fd, int &out_fd);
