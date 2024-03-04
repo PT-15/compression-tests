7e79c1910aea575970bd7e07532982ad19641e14
P-15 <dbp-15@proton.me>
2024-03-04 19:41:46 +0100

    Create main file

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include "run-length.h"
+
+int main (int argc, char *argv[])
+{
+    std::string file;
+    if (argc > 0)
+        file = argv[1];
+    else
+        file = "test.txt";
+
+    // Testing (de)compress functions for RLE (manual change)
+    rle::compress(file);
+    // rle::decompress(file);
+    return 0;
+}
