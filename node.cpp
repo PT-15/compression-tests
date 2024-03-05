f39b56fbdbcac61ab86ceb7f73f20b2db8ec8054
P-15 <dbp-15@proton.me>
2024-03-05 13:56:35 +0100

    Implement node class

---
+++ node.cpp
@@ -0,0 +1,59 @@
+#include "node.h"
+
+//////////////////
+// GENERIC NODE //
+//////////////////
+
+Node::Node (int frequency) {
+    _frequency = frequency;
+}
+
+int Node::get_frequency() const {
+    return _frequency;
+}
+
+bool Node::Compare::operator() (Node *fst, Node *snd)
+{
+    return fst->get_frequency() > snd->get_frequency();
+}
+
+///////////////
+// LEAF NODE //
+///////////////
+
+Leaf::Leaf(int frequency, char element) :
+    Node(frequency)
+{
+    _element = element;
+}
+
+bool Leaf::is_leaf() const {
+    return true;
+}
+
+char Leaf::get_element() const {
+    return _element;
+}
+
+/////////////////
+// MIDDLE NODE //
+/////////////////
+
+Group::Group (int frequency, Node *left, Node *right) :
+    Node(frequency)
+{
+    _left_child = left;
+    _right_child = right;
+}
+
+bool Group::is_leaf() const {
+    return false;
+}
+
+Node* Group::get_left_child() const {
+    return _left_child;
+}
+
+Node* Group::get_right_child() const {
+    return _right_child;
+}
---
+++ node.h
@@ -0,0 +1,38 @@
+#pragma once
+
+class Node {
+    private: 
+        int _frequency;
+
+    public:
+        Node(int frequency);
+        int get_frequency() const;
+        virtual bool is_leaf() const = 0;
+
+        class Compare {
+            public:
+                bool operator() (Node*, Node*);
+        };
+};
+
+class Leaf : public Node {
+    private:
+        char _element;
+
+    public:
+        Leaf(int frequency, char element);
+        bool is_leaf() const;
+        char get_element() const;
+};
+
+class Group : public Node {
+    private:
+        Node *_left_child;
+        Node *_right_child;
+
+    public:
+        Group(int frequency, Node *left, Node *right);
+        bool is_leaf() const;
+        Node* get_left_child() const;
+        Node* get_right_child() const;
+};
