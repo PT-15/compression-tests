b28e139d4a8b99ab8dfbc88e42f50c3506787c68
P-15 <dbp-15@proton.me>
2024-03-06 12:27:30 +0100

    Add timer class

--- tests.cpp
+++ tests.cpp
@@ -1,5 +1,7 @@
 #include "tests.h"
 
+#include <chrono>
+#include <iostream>
 
 #include "files.h"
 
@@ -11,3 +13,26 @@ void compression_ratio (const std::string original_s, const std::string compress
     printf("Compressed size: %d\n", compressed.get_size());
     printf("Compression ratio: %f\n", (float)original.get_size() / (float)compressed.get_size());
 }
+
+/////////////////
+// TIMER CLASS //
+/////////////////
+
+Timer::Timer() {}
+
+void Timer::start()
+{
+    _start = std::chrono::high_resolution_clock::now();
+}
+
+void Timer::stop()
+{
+    _stop = std::chrono::high_resolution_clock::now();
+}
+
+void Timer::print_time()
+{
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(_stop - _start);
+    std::cout << "Time: " << duration.count() << " microseconds\n";
+}
+
--- tests.h
+++ tests.h
@@ -1,5 +1,20 @@
 #pragma once
 
 #include <string>
+#include <chrono>
 
 void compression_ratio (const std::string original_s, const std::string compressed_s);
+
+class Timer {
+    private:
+        typedef std::chrono::time_point<std::chrono::system_clock, std::chrono::duration<long, std::ratio<1, 1000000000>>> chrono_time ;
+
+        chrono_time _start;
+        chrono_time _stop;
+
+    public:
+        Timer();
+        void start();
+        void stop();
+        void print_time();
+};
