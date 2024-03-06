82a6cb7d19a5d9c57a5f82ac1f7df989c79e3ce7
P-15 <dbp-15@proton.me>
2024-03-06 12:30:32 +0100

    Add compression options and tests (main)

--- main.cpp
+++ main.cpp
@@ -1,16 +1,47 @@
 #include <string>
 #include "run-length.h"
+#include "dictionary.h"
+#include "tests.h"
 
 int main (int argc, char *argv[])
 {
+    std::string mode;
     std::string file;
-    if (argc > 1)
-        file = argv[1];
-    else
-        file = "test.txt";
+    if (argc < 3) {
+        printf("Error: not enough parameters\n");
+        exit(1);
+    }
 
-    // Testing (de)compress functions for RLE (manual change)
-    rle::compress(file);
-    // rle::decompress(file);
+    mode = (std::string) argv[1];
+    file = (std::string) argv[2];
+    Timer time ;
+
+    if (mode == "-rc") {
+        time.start();
+        rle::compress(file);
+        time.stop();
+        compression_ratio(file, file + ".rle");
+        time.print_time();
+    }
+    else if (mode == "-rd") {
+        time.start();
+        rle::decompress(file);
+        time.stop();
+        time.print_time();
+    }
+    else if (mode == "-dc") {
+        time.start();
+        dict::compress(file);
+        time.stop();
+        compression_ratio(file, file + ".dc");
+        time.print_time();
+    }
+    else if (mode == "-dd") {
+        time.start();
+        dict::decompress(file, (std::string)argv[3]);
+        time.stop();
+        time.print_time();
+    }
+
     return 0;
 }
